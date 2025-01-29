67f142b161caa4825801f413a722b99d662954c6
Daniel Caspi <145657550+danielcaspi@users.noreply.github.com>
2025-01-29 12:25:56 +0200

    Update write_control_unit.v

--- asy_fifo.srcs/sources_1/new/write_control_unit.v
+++ asy_fifo.srcs/sources_1/new/write_control_unit.v
@@ -4,8 +4,8 @@ module write_control_unit(
                              input write_clk,
                              input reset_n,
                              input write_req,
-                             input [3:0] read_ptr, 
-                             output [3:0] write_ptr,   // control of write ptr 
+                             input [4:0] read_ptr, 
+                             output [4:0] write_ptr,   // control of write ptr 
                              output write,             // and write (to mem)+ full mem(out)
                              output full,
                              output of
@@ -13,37 +13,26 @@ module write_control_unit(
     
 reg of_reg;
 assign of = of_reg;
-reg [3:0] write_ptr_reg;    
-wire [3:0] inc;
-assign write_ptr = write_ptr_reg;
-assign inc = write_ptr_reg + 1;   
-assign full = (read_ptr == inc)? 1'b1:1'b0;   // full control
-assign write = write_req;        
+reg [4:0] write_ptr_reg;    
+assign write_ptr = write_ptr_reg; 
+assign full = ((read_ptr[4]^write_ptr_reg[4]) & (read_ptr[3:0] == write_ptr_reg[3:0]))? 1'b1:1'b0;   // full control
+assign write = (write_req & (!full)) ? 1'b1:1'b0;         
 
 always@(posedge write_clk or negedge reset_n)
     if(!reset_n)
     begin
         of_reg <= 0;
-        write_ptr_reg<=4'b0000;
+        write_ptr_reg<=5'b00000;
     end
-    else if (write && (!full))
+    else if (write_req && (!full))
     begin
         of_reg <= 0;
         write_ptr_reg<=write_ptr_reg+1;
     end
-    else if (write && full)
+    else if (write_req && full)
     begin
         of_reg <= 1;
     end
     
 endmodule
-
-
-
-
-
-
-
-
-
 
